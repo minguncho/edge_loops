65c1b66aa4f5bb299c102f09c9f191995b98a487
MangoShip <mingun0108@gmail.com>
2026-05-05 11:32:32 -0700

    Updated partitioner calls

--- examples/spmv/thread_mapped_edge.cu
+++ examples/spmv/thread_mapped_edge.cu
@@ -54,8 +54,10 @@ int main(int argc, char **argv) {
   generate::random::uniform_distribution(B.begin(), B.begin(), 1, 10);
 
   Partitioner<index_t, type_t, quarks_t> partitioner(A);
-  partitioner.partition_coordinate_space(1, 1, 1, A.cols);
-  //partitioner.partition_position_space(2, 2);
+  partitioner.partition_atoms_coordinate_space(2, 2);
+  partitioner.partition_tiles_coordinate_space(1, 1);
+  //partitioner.partition_atoms_position_space(2, 2);
+  //partitioner.partition_tiles_position_space(1, 1);
   partitioner.prepare_gpu();
 
   using setup_t =
